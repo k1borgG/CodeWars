28a5591c113625ca2e4dca26dd64f86a271b3ffb
k1borG <kiborg66666666@yandex.ru>
2022-02-20 22:59:04 +0300

    Made descrpition

--- CodeWars.py
+++ CodeWars.py
@@ -1,3 +1,4 @@
+# # 1) A Narcissistic Number is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base.
 # x1 = int(input())
 # x = x1
 # sum = 0
@@ -15,17 +16,17 @@
 # else:
 #     print(False)
 
-
-def validate_pin(pin):
-    count = 0
-    for s in pin:
-        if '0' <= s <= '9':
-            count += 1
-            if count > 6:
-                return False
-        else:
-            return False
-    if count == 4 or count == 6:
-        return True
-    else:
-        return False
+# # 2) ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+# def validate_pin(pin):
+#     count = 0
+#     for s in pin:
+#         if '0' <= s <= '9':
+#             count += 1
+#             if count > 6:
+#                 return False
+#         else:
+#             return False
+#     if count == 4 or count == 6:
+#         return True
+#     else:
+#         return False
